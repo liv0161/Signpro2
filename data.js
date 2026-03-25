bd72f9ab24a69103bb59ae3bd2d538cbfdc8e1fb
liv0161 <olivia777c@icloud.com>
2026-03-25 08:45:32 +0000

    Add lessons data for sign language learning

---
+++ data.js
@@ -0,0 +1,26 @@
+const lessons = [
+  {
+    id: "lesson1",
+    title: "Greetings",
+    signs: [
+      {
+        name: "Hello",
+        embed: "https://bslsignbank.ucl.ac.uk/dictionary/words/hello-1.html"
+      },
+      {
+        name: "Goodbye",
+        embed: "https://bslsignbank.ucl.ac.uk/dictionary/words/goodbye-1.html"
+      }
+    ]
+  },
+  {
+    id: "lesson2",
+    title: "Politeness",
+    signs: [
+      {
+        name: "Thank You",
+        embed: "https://bslsignbank.ucl.ac.uk/dictionary/words/thank-you-1.html"
+      }
+    ]
+  }
+];
