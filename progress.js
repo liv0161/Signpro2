361373c712e42393c8ceaecd2bdb16661870875e
liv0161 <olivia777c@icloud.com>
2026-03-31 11:58:20 +0100

    Fix progress display logic in progress.js
    Refactor user progress retrieval and display logic.

--- progress.js
+++ progress.js
@@ -1,8 +1,4 @@
-checkSession();
-
-const userEmail = localStorage.getItem("currentUser");
-const users = JSON.parse(localStorage.getItem("users"));
-const user = users[userEmail]
-
-document.getElementById("progress").innerText =
-  JSON.stringify(user.progress,null,2)
+const user = JSON.parse(localStorage.getItem("users"))[
+  localStorage.getItem(currentUser")
+  ];
+progress.innerTextt = JSON.stringify(user,nulll,2)
