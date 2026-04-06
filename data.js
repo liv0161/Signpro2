7f0077dcfd1fa200a2b29dc2daac236dca2386aa
liv0161 <olivia777c@icloud.com>
2026-04-06 14:17:20 +0100

    Update video links and remove unused lessons

--- data.js
+++ data.js
@@ -3,56 +3,8 @@ const lessons = [
     id: "lesson1",
     title: "Greetings",
     signs: [
-      {name: "Hello",video: "https://www.youtube.com/embed/gWIrN56a55I?si=LY_VazoOxIrcWzrW"},
-      {name: "Goodbye", video: "https://www.youtube.com/embed/UaF2_Q2uy-8?si=3RHO-eiAuOI2YIC6"},
-      {name: "How Are You?", video:"https://www.youtube.com/embed/s0wYgm4l2so?si=Ww2hFBCPQzioigbb"},
-      {name:"My Name Is", video: "https://www.youtube.com/embed/KVxDi1yC8_4?si=Ef5wu1PY85vNckbp"}
+      { name: "Hello", video: "https://www.youtube.com/embed/jLqplcvPnG0" },
+      { name: "Goodbye", video: "https://www.youtube.com/embed/hwA0v_Pd844" }
     ]
-  },
-  {
-    id: "lesson2",
-    title: "Manners",
-    signs: [
-      {name: "Thank You", video: "https://www.youtube.com/embed/jLqplcvPnG0?si=ALY72NCNInqUTIMy"},
-      {name: "Please", video: "https://www.youtube.com/embed/auPMG2cE5dg?si=cU9iB1qkGQbQOZdb"},
-      {name: "Sorry", video: "https://www.youtube.com/embed/SlQCAOfKvds?si=Fsu4_XFb_P3EM6EW"},
-      {name:"You're Welcome",video:"https://www.youtube.com/embed/hwA0v_Pd844?si=6oNy0hrMar3NZDPz"}
-    ]
-  },
-  {
-    id:"lesson3",
-    title:"Common Verbs",
-    signs:[
-      {name: "Eat", video:"https://www.youtube.com/embed/JGjCpTB3hoY?si=6Ms5SsTVswpteCpr"},
-      {name:"Drink",video:"https://www.youtube.com/embed/uUL1SaNlR-Q?si=tKMEyfJMDOXyemi8"},
-      {name:"Look",video:"https://www.youtube.com/embed/9R3n-gp22bg?si=2xGao1CJUQkZR3EG"},
-      {name:"Listen",video:"https://www.youtube.com/embed/TuVv1mJ9qwE?si=ubinTgOCGm_9EpPs"},
-      {name:"Stop",video:"https://www.youtube.com/embed/v3ApBdJS_d4?si=aam9iNkocaVG0N_t"}
-]
-  },
-  {
-    id:"lesson4",
-    title:"Kitchen Phrases",
-    signs:[
-      {name: "Would You Like Some Food?",video:"https://www.youtube.com/embed/n-Yn3fNANhk?si=4uj7rlX-NidDOLmx"},
-      {name:"Hungry",video:"https://www.youtube.com/embed/n-O5vqdKIKo?si=nyW-gFTZyCFKtMcJ"},
-      {name:"Sandwich",video:"https://www.youtube.com/embed/BFINI9jyK8U?si=tD6U6BgLwU4Lf6b-"},
-      {name:"Cooking",video:"https://www.youtube.com/embed/2tP65TrF2CQ?si=odLvXeEQ-mtxFeHE"},
-      {name:"Chocolate",video:"https://www.youtube.com/embed/UM7_QMdCFcA?si=EWQbeVqE_1Qs5Kvq"},
-      {name:"Crisps",video:"https://www.youtube.com/embed/KRDla78kYHE?si=KUIl_D1O7TI25IqB"}
-  ]
-  },
-  {
-    id:"lesson5",
-    title:"Questions",
-    signs:[
-      {name:"Who?",video:"https://www.youtube.com/embed/QcgS4DhxPtY?si=EevX0TNa8JqGrQdL"},
-      {name:"What?",video:"https://www.youtube.com/embed/kxag6_1TKAA?si=Tzme56wgU-jvTwpM"},
-      {name:"When?",video:"https://www.youtube.com/embed/4pHEfwqxuvs?si=tF6O-A8O2EV0W4D9"},
-      {name:"Where?",video:"https://www.youtube.com/embed/qAY6hUsCs84?si=gMKygMgxzyWK_5v0"},
-      {name:"Why?",video:"https://www.youtube.com/embed/Bk0otnfrljA?si=BBrgyb24xShMH9A2"},
-      {name:"How?",video:"https://www.youtube.com/embed/DBbuPmLpbyM?si=lefPTWnkL6VbwHeH"}
-      ]
   }
-  ];
- 
+];
