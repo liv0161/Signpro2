5676b2c7dc451e4dc03d6416dbb1941b49ee36a6
liv0161 <olivia777c@icloud.com>
2026-04-06 14:47:18 +0100

    Update lesson titles and add new signs

--- data.js
+++ data.js
@@ -1,12 +1,56 @@
 const lessons = [
   {
     id: "lesson1",
-    title: "Test Lesson",
+    title: "Greetings",
     signs: [
-      { 
-        name: "Test", 
-        video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
-      }
+      { name: "Hello", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/HE/HELLO.mp4" },
+      { name: "Goodbye", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WA/WAVE-HAND.mp4" },
+      { name: "How Are You?", video: "https://www.youtube.com/embed/s0wYgm4l2so" },
+      { name: "My Name Is", video: "https://www.youtube.com/embed/KVxDi1yC8_4" }
+    ]
+  },
+  {
+    id: "lesson2",
+    title: "Manners",
+    signs: [
+      { name: "Thank You", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/TH/THANK-YOU.mp4" },
+      { name: "Please", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/PL/PLEASE.mp4" },
+      { name: "Sorry", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/SO/SORRY.mp4" },
+      { name: "You're Welcome", video: "https://www.youtube.com/embed/hwA0v_Pd844" }
+    ]
+  },
+  {
+    id: "lesson3",
+    title: "Common Verbs",
+    signs: [
+      { name: "Eat", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/EA/EAT.mp4" },
+      { name: "Drink", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/DR/DRINK.mp4" },
+      { name: "Look", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/LO/LOOK.mp4" },
+      { name: "Listen", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/LI/LISTEN.mp4" },
+      { name: "Stop", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/ST/STOP.mp4" }
+    ]
+  },
+  {
+    id: "lesson4",
+    title: "Kitchen Phrases",
+    signs: [
+      { name: "Hungry", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/HU/HUNGRY.mp4" },
+      { name: "Sandwich", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/SA/SANDWICH.mp4" },
+      { name: "Cooking", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/CO/COOK.mp4" },
+      { name: "Chocolate", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/CH/CHOCOLATE.mp4" },
+      { name: "Crisps", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/CR/CRISPS.mp4" }
+    ]
+  },
+  {
+    id: "lesson5",
+    title: "Questions",
+    signs: [
+      { name: "Who?", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WH/WHO.mp4" },
+      { name: "What?", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WH/WHAT.mp4" },
+      { name: "When?", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WH/WHEN.mp4" },
+      { name: "Where?", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WH/WHERE.mp4" },
+      { name: "Why?", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WH/WHY.mp4" },
+      { name: "How?", video: "https://bslsignbank.ucl.ac.uk/media/bsl-video/HO/HOW.mp4" }
     ]
   }
 ];
