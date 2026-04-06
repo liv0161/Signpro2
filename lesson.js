ff61fb80440e0354dc10ac2cfc015418eeab194e
liv0161 <olivia777c@icloud.com>
2026-04-06 14:20:31 +0100

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -1,40 +1,25 @@
-document.addEventListener("DOMContentLoaded", () =>{
-  constlessonId = localStorage.getItem("lesson");
-  const lesson = lessons.find(1 => 1.id === lesonId);
-  if (!lesson){
-    alert("Lesson not found");
-    window.location.href = "course.html"
-    return;
-  }
-  let i = 0;
-  let correct = 0;
-  function load(){
-    const s = lesson.signs[i];
-    document.getElementById("lessonTitle").innerText = lesson.title;
-    document.getElementById("counter").innerText = `${i+1}/${lesson.signs.length}`;
-    document.getElementById("name").innerText = s.name;
+const lesson = lessons[0];
 
-  // forc relad jic it doesnt work
-    const video = document.getElementById("video");
-    video.src = s.video;
-  }
-  window.next = function(){
-    load();
-  } else{
-    complete();
-  }
-};
-function complete(){
-  const users = JSON.parse(localStorage.getItem("users")) || {};
-  const user = users[localStorage.getItem("currentUser");
-  if(!user.progress) user.progress ={};
-  user.progress[lesson.id= {
-    completed:true,
-    accuracy:10
-  };
-  localStorage.setItem("users",JSON.stringify(uses));
-  alert("lesson complete");
-  window.location.href=""course.html";
+let i = 0;
+
+function load() {
+  const sign = lesson.signs[i];
+
+  document.getElementById("lessonTitle").innerText = lesson.title;
+  document.getElementById("counter").innerText = (i + 1) + "/" + lesson.signs.length;
+  document.getElementById("name").innerText = sign.name;
+
+  document.getElementById("video").src = sign.video;
 }
+
+function next() {
+  i++;
+
+  if (i < lesson.signs.length) {
+    load();
+  } else {
+    alert("Finished!");
+  }
+}
+
 load();
-};
