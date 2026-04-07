2bb292bdccb725f1285d3cf4b8fb4cdb7ad8eb42
liv0161 <olivia777c@icloud.com>
2026-04-07 17:23:39 +0100

    Update practice.js

--- practice.js
+++ practice.js
@@ -4,7 +4,6 @@ function pickRandom() {
   if (!lessons || lessons.length === 0) {
     document.getElementById("feedback").innerText = "No lessons found";
     return;
-  showOptions(sign.name);
   }
 
   const allSigns = lessons.flatMap(l => l.signs);
@@ -22,8 +21,10 @@ function pickRandom() {
   video.src = "";
   video.src = sign.video;
   video.load();
+  // for mcq options
+  showOptions(sign.name);
 }
-
+// checks users typed answer
 function checkAnswer() {
   const input = document.getElementById("answer").value.toLowerCase();
 
@@ -35,22 +36,26 @@ function checkAnswer() {
     document.getElementById("feedback").innerText =
       "Wrong! Correct answer: " + currentSign.name;
   }
+  document.getElementById("answer").value="";
+}
+// changes displayed sign
 function nextSign() {
   document.getElementById("answer").value = "";
   document.getElementById("feedback").innerText = "";
   pickRandom();
 }
+// for multiple choice questions
 function showOptions(correctAnswer) {
   const allSigns = lessons.flatMap(l => l.signs);
   const options = [correctAnswer];
-
+  // makes 3 random incorrect answers
   while (options.length < 4) {
     const random = allSigns[Math.floor(Math.random() * allSigns.length)].name;
     if (!options.includes(random)) {
       options.push(random);
     }
   }
-
+// shuffle options
   options.sort(() => Math.random() - 0.5);
 
   const container = document.getElementById("options");
@@ -62,6 +67,7 @@ function showOptions(correctAnswer) {
     `;
   });
 }
+// multiple choice selection 
 function selectOption(choice) {
   if (choice === currentSign.name) {
     document.getElementById("feedback").innerText = "Correct!";
@@ -74,10 +80,5 @@ function selectOption(choice) {
     nextSign();
   }, 1000);
 }
-
-  document.getElementById("answer").value = "";
-
-  pickRandom();
-}
-// loads page first
+// loads first sign
 window.onload = pickRandom;
